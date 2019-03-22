274b53313e8f09e7c4ce368cf0a135cf971ba44c
mhma <mhma@cookapps.com>
2019-03-22 14:52:39 +0900

    Step3. Disqus

---
+++ docs/.vuepress/components/Disqus.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="disqus_thread" style="padding-top: 0px;"></div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      var disqus_config = function () {
+        this.page.url = window.location.origin;
+        this.page.identifier = window.location.pathname;
+      };
+      (function() {
+        var d = window.document, s = d.createElement('script');
+        s.src = 'https://cookapps.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', + new Date());
+        (d.head || d.body).appendChild(s);
+      })();
+    }
+  }
+</script>
---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,13 @@
+export default ({ router }) => {
+    router.afterEach((to, from) => {
+      if (from.path !== to.path) {
+        // ignore frontmattter disqus reload
+        if (to.path != '/') {
+          if (typeof window !== 'undefined' && window.DISQUS) {
+            setTimeout(() => window.DISQUS.reset({ reload: true }), 0)
+          }
+        }
+      }
+    })
+  }
+  
